66228db2a3b524f2f2f7e2f444e92b4bc3f67f67
Laila <Laila.elmallass.2018@gmail.com>
2025-01-31 19:28:47 +0100

    added base in viteConfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Sweet_Donuts/',  // This sets the base path for all assets
   plugins: [react()],
 })
